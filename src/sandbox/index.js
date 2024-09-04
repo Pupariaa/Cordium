c610f98101feaf4a288d8590bf587e17cef180ec
lakazatong <Bo_wood@outlook.fr>
2024-09-04 19:59:16 +0200

    idk

--- src/sandbox/index.js
+++
@@ -1,6 +0,0 @@
-console.log('--------------------------------------------SANDBOX OUTPUT START--------------------------------------------')
-const fs = require('fs');
-const path = require('path');
-const guild = global.client.guilds.cache.get(process.env.discord_guid);
-
-global.Channel.send('general', 'content')
