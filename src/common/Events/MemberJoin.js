54f8fc2d4dcc455e0269a9bd23e79951dc3e9e80
Maxime <32194766+Pupariaa@users.noreply.github.com>
2024-09-06 09:13:32 +0200

    Update MemberJoin.js

--- src/common/Events/MemberJoin.js
+++ src/common/Events/MemberJoin.js
@@ -6,7 +6,12 @@ const filePath = path.resolve(__dirname, 'guildMemberAdd.js');
 
 global.client.on('guildMemberAdd', async member => {
     try {
-       
+       if(process.env.membercount){
+        let count = global.client.getMemberCount(false)
+            //TODO: global.Channel.rename('membercount', ?)
+       }
+
+
     } catch (error) {
         console.error(`${filePath} - Line ${__line} (guildMemberAdd): Error handling guild member add:`, error);
     }
