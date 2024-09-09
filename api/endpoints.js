e5221fe21f7b96bcc251d996d7741aede1bdf6f7
Maxime <32194766+Pupariaa@users.noreply.github.com>
2024-09-09 19:17:28 +0200

    add endpoint to api

--- api/endpoints.js
+++ api/endpoints.js
@@ -7,6 +7,14 @@ const endpoints = [
             { name: 'userid', type: 'int', mandatory: true },
         ]
     },
+    {
+        name: 'get_message',
+        type: 'private',
+        params: [
+            { name: 'key', type: 'string', mandatory: true, length: 32 },
+            { name: 'id', type: 'int', mandatory: true },
+        ]
+    },
 ];
 
 module.exports = endpoints;
