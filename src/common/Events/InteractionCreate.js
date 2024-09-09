a9f244982e592b72a815c2f19d6632dfe606f3f1
lakazatong <Bo_wood@outlook.fr>
2024-09-09 15:34:46 +0200

    Update InteractionCreate.js

--- src/common/Events/InteractionCreate.js
+++ src/common/Events/InteractionCreate.js
@@ -3,16 +3,16 @@
 require('puparia.getlines.js');
 const reportEvent = Events.createReportEvent(__filename);
 
-const default_handler = (interaction, eventName) => {
+const event = Events.InteractionCreate;
+let eventName = String(event);
+
+const default_handler = (interaction) => {
     try {
         reportEvent(__line, eventName, 'author.name', interaction.user.tag, 'client.name', interaction.client.user.tag, 'channel.name', interaction.channel.name);
     } catch(err) {
         console.error(`${__filename} - Line ${__line} (${eventName}): `, err);
     }
 } 
-
-const event = Events.InteractionCreate;
-let eventName = String(event);
 
 function reportChatInputCommand(interaction) {
     const functionName = 'reportChatInputCommand';
@@ -41,42 +41,42 @@ global.client.on(event, async (interaction) => {
 
     if (interaction.isAutocomplete()) {
         eventName += '.autocomplete';
-        default_handler(interaction, eventName);
+        default_handler(interaction);
     } else if (interaction.isButton()) {
         eventName += '.button';
-        default_handler(interaction, eventName);
+        default_handler(interaction);
     } else if (interaction.isMessageComponent()) {
         eventName += '.messageComponent';
-        default_handler(interaction, eventName);
+        default_handler(interaction);
     } else if (interaction.isModalSubmit()) {
         eventName += '.modalSubmit';
-        default_handler(interaction, eventName);
+        default_handler(interaction);
     } else if (interaction.isChatInputCommand()) {
         reportChatInputCommand(interaction);
     } else if (interaction.isUserContextMenuCommand()) {
         eventName += '.userContextMenuCommand';
-        default_handler(interaction, eventName);
+        default_handler(interaction);
     } else if (interaction.isContextMenuCommand()) {
         eventName += '.contextMenuCommand';
-        default_handler(interaction, eventName);
+        default_handler(interaction);
     } else if (interaction.isMessageContextMenuCommand()) {
         eventName += '.messageContextMenuCommand';
-        default_handler(interaction, eventName);
+        default_handler(interaction);
     } else if (interaction.isStringSelectMenu()) {
         eventName += '.stringSelectMenu';
-        default_handler(interaction, eventName);
+        default_handler(interaction);
     } else if (interaction.isUserSelectMenu()) {
         eventName += '.userSelectMenu';
-        default_handler(interaction, eventName);
+        default_handler(interaction);
     } else if (interaction.isRoleSelectMenu()) {
         eventName += '.roleSelectMenu';
-        default_handler(interaction, eventName);
+        default_handler(interaction);
     } else if (interaction.isMentionableSelectMenu()) {
         eventName += '.mentionableSelectMenu';
-        default_handler(interaction, eventName);
+        default_handler(interaction);
     } else if (interaction.isChannelSelectMenu()) {
         eventName += '.channelSelectMenu';
-        default_handler(interaction, eventName);
+        default_handler(interaction);
     } else {
         console.error(`${__filename} - Line ${__line} (${eventName}): unknown interaction of type "${interaction.type}"`);
     }
