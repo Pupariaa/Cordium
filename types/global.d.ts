87d9684cdf8bdc22668bceede9b9571c1c61f7e7
lakazatong <Bo_wood@outlook.fr>
2024-09-10 07:12:37 +0200

    removed ts bs

--- types/global.d.ts
+++
@@ -1,8 +0,0 @@
-// global.d.ts
-declare namespace NodeJS {
-    interface Global {
-      Channel: import('../src/common/Statics').Channels;
-      attachment: import('../src/common/AttachmentManager').AttachmentManager
-    }
-  }
-  
