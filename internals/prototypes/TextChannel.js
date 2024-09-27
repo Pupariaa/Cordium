84a611f3abea26446ceff879c49a143c56a092b9
lakazatong <Bo_wood@outlook.fr>
2024-09-27 04:13:42 +0200

    added getMessageById in TextChannel prototype

--- internals/prototypes/TextChannel.js
+++ internals/prototypes/TextChannel.js
@@ -1,10 +1,16 @@
 'use strict';
 const { TextChannel } = require('discord.js');
 
-TextChannel.prototype.fetchAllMessages = function (...args) { return global.channels.fetchAllMessages.call(this, ...args); }
+TextChannel.prototype.fetchAllMessages = function (...args) {
+    return global.channels.fetchAllMessages.call(this, ...args);
+};
 
 TextChannel.prototype.hasTag = function (tag) {
     this._hasTag(global.channels.text, tag);
+};
+
+TextChannel.prototype.getMessageById = function (id) {
+    return this.messages.cache.get(id);
 };
 
 module.exports = {};
