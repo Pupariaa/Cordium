e5ffbf1740e7ec9f24cfe8ac2eef7abf2a9427a8
Maxime <32194766+Pupariaa@users.noreply.github.com>
2025-10-12 15:45:10 +0200

    Remove deleted messages from cache

--- src/events/Message/MessageDelete.js
+++ src/events/Message/MessageDelete.js
@@ -4,5 +4,8 @@ module.exports = {
 	listen: true,
 	report: true,
 	callback: async function (message) {
+		if (global.messagesCache) {
+			await global.messagesCache.deleteMessage(message.id);
+		}
 	}
 }
