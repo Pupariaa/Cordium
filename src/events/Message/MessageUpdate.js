ffb9a742a1c3c0f1d50886e27ce75681ee5f954a
Maxime <32194766+Pupariaa@users.noreply.github.com>
2025-10-12 15:44:42 +0200

    Track message edits history and update message cache on edit

--- src/events/Message/MessageUpdate.js
+++ src/events/Message/MessageUpdate.js
@@ -4,5 +4,17 @@ module.exports = {
 	listen: true,
 	report: true,
 	callback: async function (oldMessage, newMessage) {
+		if (global.messagesCache && oldMessage.content !== newMessage.content) {
+			// Store edit history
+			await global.messagesCache.addMessageEdit(
+				newMessage.id,
+				oldMessage.content || '',
+				newMessage.content || '',
+				newMessage.editedTimestamp || Date.now()
+			);
+
+			// Update the message in cache
+			await global.messagesCache.updateMessage(newMessage);
+		}
 	}
 }
