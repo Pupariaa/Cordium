c25cfd02b14212af4416a3c1b638686cb2f0acb3
Maxime <32194766+Pupariaa@users.noreply.github.com>
2025-10-12 15:42:07 +0200

    Store messages and attachments in cache on message creation

--- src/events/Message/MessageCreate.js
+++ src/events/Message/MessageCreate.js
@@ -4,5 +4,12 @@ module.exports = {
 	listen: true,
 	report: true,
 	callback: async function (message) {
+		if (global.saveAttachments && message.attachments.size > 0) {
+			await global.attachmentsManager.saveAttachments(message);
+		}
+
+		if (global.messagesCache) {
+			await global.messagesCache.addMessage(message);
+		}
 	}
 }
