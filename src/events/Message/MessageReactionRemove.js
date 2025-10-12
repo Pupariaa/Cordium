748174c592eed1d3c92b986566994d12bd450b89
Maxime <32194766+Pupariaa@users.noreply.github.com>
2025-10-12 15:46:22 +0200

    Track reaction removals and update message cache

--- src/events/Message/MessageReactionRemove.js
+++ src/events/Message/MessageReactionRemove.js
@@ -4,5 +4,20 @@ module.exports = {
 	listen: true,
 	report: true,
 	callback: async function (reaction, user, details) {
+		if (global.messagesCache) {
+			// Store reaction event
+			await global.messagesCache.addReactionEvent(
+				reaction.message.id,
+				user.id,
+				user.username,
+				reaction.emoji.name,
+				reaction.emoji.id,
+				'remove',
+				Date.now()
+			);
+
+			// Update message reactions in cache
+			await global.messagesCache.updateMessage(reaction.message);
+		}
 	}
 }
